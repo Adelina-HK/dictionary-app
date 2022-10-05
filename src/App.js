2b354c88df9a3e3eb2658d9b366409e2f66ca1ce
Adelina <adelhetman@gmail.com>
2022-10-05 14:27:22 +0300

    Created a new component - Dictionary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
+import book from "./book.png";
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-        <p>Hello from Adelina`s dictionary 😉 </p>{" "}
+        <h1 className="m-2"><img src={book} className="img-book m-2" alt="book pic" /> 
+          Dictionary </h1>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center"><small>Coded by Adelina Hetman</small></footer>
+    </div>
     </div>
   );
 }
