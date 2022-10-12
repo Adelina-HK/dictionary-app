94e092d7e75f97578baa2adc121515dde71d9f7c
Adelina <adelhetman@gmail.com>
2022-10-12 13:30:20 +0300

    Created a new component - Synonyms.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,17 +1,20 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
     <div className="Meaning mt-2">
       <h3>{props.meaning.partOfSpeech}</h3>
-
       {props.meaning.definitions.map(function(definition, index) {
         return (
           <div key={index}>
             <p>
-              {definition.definition}
+              <strong>Definition: </strong> {definition.definition}
               <br />
+              <strong>Example: </strong>
               <em>{definition.example}</em>
+              <br />
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
--- src/Results.js
+++ src/Results.js
@@ -3,28 +3,31 @@ import "./Results.css";
 import Meaning from "./Meaning";
 
 export default function Results(props) {
- if(props.results) {
-   return (
-    <div className="Results">
-      <div className="row keyword">
-        <div className="col-sm-6">
-          <h2>{props.results.word}</h2>
+  if (props.results) {
+    return (
+      <div className="Results">
+        <div className="row keyword">
+          <div className="col-sm-6">
+            <h2>{props.results.word}</h2>
+          </div>
+          <div className="col-sm-6">
+            <h2>{props.results.phonetic}</h2>
+          </div>
         </div>
-        <div className="col-sm-6">
-          <h2>{props.results.phonetic}</h2>
+        <div className="meanings">
+          <h3>
+            {props.results.meanings.map(function(meaning, index) {
+              return (
+                <div key={index}>
+                  <Meaning meaning={meaning} />
+                </div>
+              );
+            })}
+          </h3>
         </div>
       </div>
-      <div className="meanings">
-        <h3>{props.results.meanings.map(function(meaning, index){
-          return (
-            <div key={index}>
-              <Meaning meaning={meaning}/>
-            </div>
-          )
-        })}</h3>
-      </div>
-    </div>
-  );
-      } else { return null;
-      }
-    }
+    );
+  } else {
+    return null;
+  }
+}
