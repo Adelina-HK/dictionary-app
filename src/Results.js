b781b7bf37cc8571c062d95d4c978669112e18b6
Adelina <adelhetman@gmail.com>
2022-10-13 17:07:05 +0300

    Created a new component - Phonetic.js

--- src/Results.js
+++ src/Results.js
@@ -1,17 +1,27 @@
 import React from "react";
 import "./Results.css";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
         <div className="row keyword">
-          <div className="col-sm-6">
+          <div className="col-sm-5">
             <h2>{props.results.word}</h2>
           </div>
-          <div className="col-sm-6">
+          <div className="col-sm-5">
             <h2>{props.results.phonetic}</h2>
+          </div>
+          <div className="col-sm-2">
+            {props.results.phonetics.map( function (phonetic, index){
+              return(
+                <div key={index}>
+                  <Phonetic phonetic={phonetic} />
+                </div>
+              );
+            })}
           </div>
         </div>
         <div className="meanings">
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,13 +1,20 @@
 import React from "react";
 
 export default function Synonyms(props) {
-  if (props.synonyms) {
+   if (props.synonyms) {
     return (
-      <ul className="Synonyms">
-        {props.synonyms.map(function(synonyms, index) {
-          return <li key={index}> {synonyms} </li>;
-        })}
-      </ul>
+      <div className="Synonyms">
+        <ul>
+          {props.synonyms.map(function(synonyms, index) {
+            return (
+              <li key={index}>
+                <strong>Synonyms: </strong>
+                {synonyms}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     );
   } else {
     return null;
