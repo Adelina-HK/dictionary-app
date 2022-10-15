0203fbba62fc896fcd1302b47d4fcf47b47bf22d
Adelina <adelhetman@gmail.com>
2022-10-16 01:35:37 +0300

    Created a new component - Photos, second API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,13 +2,18 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState("");
+  let [photos, setPhotos] = useState("");
 
   function handleResponse(response) {
     setResults(response.data[0]);
+  }
+  function handlePexelResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
@@ -16,6 +21,15 @@ export default function Dictionary() {
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsAPIKey =
+      "563492ad6f9170000100000177cba5282e9d4c028b3957c843456409";
+    let pexelsAPIUrl = `https://api.pexels.com/v1/search?query=${keyword}`;
+    axios
+      .get(pexelsAPIUrl, {
+        headers: { Authorization: `Bearer ${pexelsAPIKey}` },
+      })
+      .then(handlePexelResponse);
   }
 
   function handleKeywordChange(event) {
@@ -35,6 +49,7 @@ export default function Dictionary() {
       </form>
       <div className="hint">suggested words: forest, sunset, yoga,..</div>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
---
+++ src/Photos.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Photos.css";
+
+export default function Photos(props) {
+  console.log(props.photos);
+  if (props.photos) {
+    return (
+      <section className="Photos">
+        <div className="row">
+          {props.photos.map(function(photo, index) {
+            return (
+              <div className="col-4">
+                <a
+                  href={photo.src.original}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <img
+                    src={photo.src.medium}
+                    className="img-fluid"
+                    alt="images"
+                    key={index}
+                  />
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    );
+  } else {
+    return null;
+  }
+}
